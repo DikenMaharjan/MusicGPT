48e9a4b924ee93879605d82943b0201d1310e71e
Diken Maharjan <dikenmaharjan10@gmail.com>
2025-09-08 16:06:10 +0545

    feat: Add strings for generate content top bar

--- app/src/main/java/com/example/musicplayer/feature/generate_content/component/GenerateContentTopBar.kt
+++ app/src/main/java/com/example/musicplayer/feature/generate_content/component/GenerateContentTopBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicplayer.R
 import com.example.musicplayer.ui_core.insets.rememberZeroWindowInsets
@@ -21,11 +22,11 @@ fun GenerateContentTopBar(
         navigationIcon = {
             Image(
                 painter = painterResource(R.drawable.ic_launcher_foreground),
-                contentDescription = "App Logo"
+                contentDescription = stringResource(R.string.generate_content_top_bar_app_logo_description)
             )
         },
         title = {
-            Text("MusicGPT")
+            Text(stringResource(R.string.generate_content_top_bar_app_title))
         },
         windowInsets = rememberZeroWindowInsets()
     )

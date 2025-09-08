ef55c4faf36c05af0aba0635e7be6679d3943874
Diken Maharjan <dikenmaharjan10@gmail.com>
2025-09-08 15:38:23 +0545

    feat: Apply navigationBarsPadding to bottom bar

--- app/src/main/java/com/example/musicplayer/navigation/component/bottom_bar/AppBottomBar.kt
+++ app/src/main/java/com/example/musicplayer/navigation/component/bottom_bar/AppBottomBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -27,6 +28,7 @@ fun AppBottomBar(
         modifier = modifier
             .fillMaxWidth()
             .background(LocalThemeColor.current.primary.p50)
+            .navigationBarsPadding()
     ) {
         Box(
             modifier = Modifier

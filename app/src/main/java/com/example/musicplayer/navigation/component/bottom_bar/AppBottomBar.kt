7cb8797e1ea1adb938a59ca67ef861dbce412f19
Diken Maharjan <dikenmaharjan10@gmail.com>
2025-09-08 08:29:23 +0545

    feat: create AppBottomBar.kt

---
+++ app/src/main/java/com/example/musicplayer/navigation/component/bottom_bar/AppBottomBar.kt
@@ -0,0 +1,84 @@
+package com.example.musicplayer.navigation.component.bottom_bar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.musicplayer.ui.theme.LocalSpacing
+import com.example.musicplayer.ui.theme.LocalThemeColor
+
+@Composable
+fun AppBottomBar(
+    modifier: Modifier = Modifier,
+    selectedRoute: Any
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(LocalThemeColor.current.primary.p50)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(LocalSpacing.current.dimen1)
+                .background(LocalThemeColor.current.primary.p250)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(LocalSpacing.current.dimen24)
+                .padding(horizontal = LocalSpacing.current.dimen8),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            bottomNavigationItems.forEach { item ->
+                BottomNavigationIcon(
+                    isSelected = selectedRoute == item.route,
+                    icon = item.icon,
+                    contentDescription = item.contentDescription,
+                    onSelected = {}
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomNavigationIcon(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    icon: Painter,
+    contentDescription: String,
+    onSelected: () -> Unit,
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onSelected
+    ) {
+        Icon(
+            painter = icon,
+            contentDescription = contentDescription,
+            tint = if (isSelected) LocalThemeColor.current.white else LocalThemeColor.current.primary.p200
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun AppBottomBarPreview() {
+    AppBottomBar(
+        selectedRoute = GenerateContentRoute
+    )
+}
---
+++ app/src/main/java/com/example/musicplayer/navigation/component/bottom_bar/BottomNavigationItem.kt
@@ -0,0 +1,48 @@
+package com.example.musicplayer.navigation.component.bottom_bar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.musicplayer.R
+
+@Immutable
+data class BottomNavigationItem(
+    val route: Any,
+    val icon: Painter,
+    val contentDescription: String,
+)
+
+
+val bottomNavigationItems
+    @Composable
+    get() = listOf(
+        BottomNavigationItem(
+            route = GenerateContentRoute,
+            icon = painterResource(R.drawable.generate_ai_icon),
+            contentDescription = stringResource(R.string.bottom_bar_generate_ai_icon_description)
+        ),
+        BottomNavigationItem(
+            route = TempRoute,
+            icon = painterResource(R.drawable.discover_icon),
+            contentDescription = stringResource(R.string.bottom_bar_discover_icon_description)
+        ),
+        BottomNavigationItem(
+            route = TempRoute,
+            icon = painterResource(R.drawable.reel_icon),
+            contentDescription = stringResource(R.string.bottom_bar_reel_icon_description)
+        ),
+        BottomNavigationItem(
+            route = TempRoute,
+            icon = painterResource(R.drawable.user_icon),
+            contentDescription = stringResource(R.string.bottom_bar_user_icon_description)
+        )
+    )
+
+
+// A temporary route for each screen till each screen is implemented
+data object TempRoute
+
+// A temporary route for generate content screen
+data object GenerateContentRoute

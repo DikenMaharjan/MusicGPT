0277b8259e1e7e71ee9111d0e3f527308caf519d
Diken Maharjan <dikenmaharjan10@gmail.com>
2025-09-08 08:37:57 +0545

    feat: Change status bar color according to theme

--- app/src/main/java/com/example/musicplayer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/musicplayer/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.example.musicplayer.ui.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -29,7 +33,11 @@ fun MusicPlayerTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
+    val view = LocalView.current
+    SideEffect {
+        val window = (view.context as Activity).window
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+    }
     CompositionLocalProvider(
         LocalThemeColor provides themeColors,
         LocalSpacing provides spacing,

648861d566da55fd1a8a79344ec8d765dd9b83a2
Diken Maharjan <dikenmaharjan10@gmail.com>
2025-09-09 22:37:20 +0545

    feat: Add material icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    
+    implementation(libs.androidx.material.icons)
+
     // Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)

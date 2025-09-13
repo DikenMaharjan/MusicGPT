f76555b0720d2e41c146a0722487b1cab6a047e3
Diken Maharjan <dikenmaharjan10@gmail.com>
2025-09-13 19:32:26 +0545

    Refactor: Update MusicAlbumView placeholder and error icons

--- app/src/main/java/com/example/musicplayer/feature/generate_music/ui/component/MusicAlbumView.kt
+++ app/src/main/java/com/example/musicplayer/feature/generate_music/ui/component/MusicAlbumView.kt
@@ -1,19 +1,18 @@
 package com.example.musicplayer.feature.generate_music.ui.component
 
 import androidx.compose.foundation.background
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.MusicNote
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
 import coil3.compose.AsyncImage
+import com.example.musicplayer.R
 import com.example.musicplayer.feature.generate_music.data.model.Music
 import com.example.musicplayer.ui.theme.LocalThemeColor
 
 @Composable
- fun MusicAlbumView(
+fun MusicAlbumView(
     modifier: Modifier = Modifier,
     music: Music
 ) {
@@ -23,8 +22,8 @@ import com.example.musicplayer.ui.theme.LocalThemeColor
             .background(LocalThemeColor.current.primary.p1000),
         model = music.image,
         contentDescription = null,
-        fallback = rememberVectorPainter(Icons.Outlined.MusicNote),
-        placeholder = rememberVectorPainter(Icons.Outlined.MusicNote),
-        error = rememberVectorPainter(Icons.Outlined.MusicNote),
+        fallback = painterResource(R.drawable.property_1_100),
+        placeholder = painterResource(R.drawable.property_1_100),
+        error = painterResource(R.drawable.property_1_100),
     )
 }
